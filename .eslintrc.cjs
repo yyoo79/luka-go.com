bcc3a9982e4d06ad7fa7b0dfa864049f11524fa3
yyoo79 <yangq79@gmail.com>
2025-04-26 22:09:43 -0400

    init hello world

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+/** @type { import("eslint").Linter.Config } */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+  },
+};
---
+++ src/pages/Home.tsx
@@ -0,0 +1,5 @@
+function Home() {
+  return <h1>Luka Go Online</h1>;
+}
+
+export default Home;
